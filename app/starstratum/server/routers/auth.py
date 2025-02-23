d8310c331918e437939b4d94615b83598d7eb94c
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-02-23 19:13:06 -0300

    feat: obtaing hashed password in register endpoint

--- app/starstratum/server/routers/auth.py
+++ app/starstratum/server/routers/auth.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from ..models.user import User
 from dotenv import dotenv_values
+import bcrypt
 
-config = dotenv_values(".env")
+config = dotenv_values("../.env")
 
 router = APIRouter()
 
@@ -16,3 +17,12 @@ def register_user(request: Request, userInfo: User):
     )
     if user:
         print('User with given name already exists!')
+
+    salt = bcrypt.gensalt()
+    bytes_password = userInfo.password.encode('utf-8')
+    hash_password = bcrypt.hashpw(
+        password=bytes_password,
+        salt=salt
+    )
+    user_passwrd = bytes_password.decode('utf-8')
+    return {'actual_password': user_passwrd, 'hashed_password': hash_password}
