8824628dbcce844f9acd42ada94e4e7615e20a6e
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-02-23 21:22:33 -0300

    feat: insert user to mongo dtabase in register endpoint

--- app/starstratum/server/routers/auth.py
+++ app/starstratum/server/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status
+from fastapi.encoders import jsonable_encoder
 from ..models.user import User
 from dotenv import dotenv_values
 import bcrypt
@@ -11,12 +12,14 @@ router = APIRouter()
 @router.post("/register", response_description="Register user in Database", status_code=status.HTTP_201_CREATED)
 def register_user(request: Request, userInfo: User):
     database =  request.app.database[config["DB_NAME"]]
+    #userInfo = jsonable_encoder(userInfo)
 
-    user = database.find_one(
-        {"name": userInfo.name}
-    )
-    if user:
-        print('User with given name already exists!')
+    #user = database.find_one(
+    #    {"name": userInfo.name}
+    #)
+    #if use:
+    #    print('User with given name already exists!')
+    #    return
 
     salt = bcrypt.gensalt()
     bytes_password = userInfo.password.encode('utf-8')
@@ -24,5 +27,11 @@ def register_user(request: Request, userInfo: User):
         password=bytes_password,
         salt=salt
     )
-    user_passwrd = bytes_password.decode('utf-8')
-    return {'actual_password': user_passwrd, 'hashed_password': hash_password}
+    user_password = bytes_password.decode('utf-8')
+
+
+
+    userInfo.password = hash_password
+    userInfo = jsonable_encoder(userInfo)
+    created_user = database.insert_one(userInfo)
+    return created_user
