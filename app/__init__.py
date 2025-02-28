99ff24b2632a00d6eba50645ed42c0ed0477041e
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-02-28 19:14:25 -0300

    refactor: simplify file in wich server instance is created

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,21 @@
-from . import server
+
+from fastapi import FastAPI
+from .routers import auth
+from .database.connection import register_db_connection
+from .middlewares.middlewares import register_middlewares
+
+
+
+
+def create_app() -> FastAPI:
+
+    app = FastAPI()
+    register_db_connection(app)
+    register_middlewares(app)
+    app.include_router(auth.router, tags=["auth"], prefix="/auth")
+
+    return app
+
+
+app = create_app()
+
