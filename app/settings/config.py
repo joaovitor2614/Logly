2d1b953a32be43f723aaac3e0eabc297a63dcd89
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-03-01 11:01:35 -0300

    fix: load secret variable to enviroment in settings.py

--- app/settings/config.py
+++ app/settings/config.py
@@ -1,11 +1,12 @@
 
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 from pydantic import Field
 
-
+load_dotenv()
 
 class Settings(BaseSettings):
-    MONGO_DB_ATLAS_URI: str
+    MONGO_DB_ATLAS_URI: str = "mongodb+srv://<username>:<password>@cluster0.q5szy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     SECRET_KEY: str = "015a42020f023ac2c3eda3d45fe5ca3fef8921ce63589f6d4fcdef9814cd7fa7"
     JWT_ALGORITHM: str = "HS256"
     DB_NAME: str = "Users"
