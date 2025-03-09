a5d83eb6eefd0d1c68d8e3e189a79a368659ae17
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-03-09 13:21:06 -0300

    refactor: professor model id is uuid now

--- app/models/professor.py
+++ app/models/professor.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel, Field
 import uuid
 
 class Professor(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
     name: str = Field(...)
     image: str = Field(...)
     disciplines: List[str] = Field(...)
