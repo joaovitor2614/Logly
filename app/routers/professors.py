ceee78091bf6a2cf5ef19b85f3d6f740220fb0a8
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-03-15 12:13:08 -0300

    feat: add DELETE endpoint in professors router

--- app/routers/professors.py
+++ app/routers/professors.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Body, Request, Response, HTTPException, status, D
 from fastapi.encoders import jsonable_encoder
 from ..utils.security import get_current_user
 from ..models.professor import Professor
+from bson.objectid import ObjectId
 from app.settings import APP_SETTINGS
 from faker import Faker
 from randomuser import RandomUser
@@ -31,6 +32,7 @@ def create_professor(request: Request, new_professor: Professor, user_id: str = 
     created_new_professor["_id"] = str(created_new_professor["_id"])
     return created_new_professor
     
+    
 @router.get("/", response_description="Get professors data in Database", status_code=status.HTTP_200_OK)
 async def get_professors(request: Request, user_id: str = Depends(get_current_user)):
     professors_database = request.app.database[APP_SETTINGS.PROFESSORS_DB_NAME]
@@ -38,6 +40,16 @@ async def get_professors(request: Request, user_id: str = Depends(get_current_us
     for professor in professors:
         professor["_id"] = str(professor["_id"])
     return professors
+
+@router.delete("/{id}", response_description="Delete a professors")
+def delete_book(id: str, request: Request, response: Response):
+    delete_result = request.app.database[APP_SETTINGS.PROFESSORS_DB_NAME].delete_one({"_id": (ObjectId(id))})
+
+    if delete_result.deleted_count == 1:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Professor with ID {id} not found")
 
 @router.post("/test/{amount}", response_description="Post fake professors data in Database", status_code=status.HTTP_200_OK)
 async def create_fake_professors(request: Request, amount: int, user_id: str = Depends(get_current_user)):
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -9,4 +9,5 @@ import pytest
 @pytest.fixture
 def app():
     app = create_app(is_test_mode=True)
+
     return app
