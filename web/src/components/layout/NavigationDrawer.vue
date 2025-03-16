866fa119a64fb8377fb54177b2251814efa729a3
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-03-15 21:31:28 -0300

    feat: display user name in nav drawer

--- web/src/components/layout/NavigationDrawer.vue
+++ web/src/components/layout/NavigationDrawer.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { useAuthStore } from '../../stores/index';
+import { useAuthStore, useUserStore } from '../../stores/index';
+
 import { router } from '../../router/router'
 const authStore = useAuthStore();
+const userStore = useUserStore()
 </script>
 
 <template>
@@ -14,30 +16,23 @@ const authStore = useAuthStore();
             prepend-avatar="https://randomuser.me/api/portraits/women/75.jpg"
             nav
           ></v-list-item>
-          <v-list-item>João Vitor</v-list-item>
+          <v-list-item>Welcome {{ userStore.userInfo.name }}</v-list-item>
           <v-divider></v-divider>
   
           <v-list
             density="compact"
             nav
           >
-            <v-list-item prepend-icon="mdi-view-dashboard" value="dashboard"></v-list-item>
-  
-            <v-list-item prepend-icon="mdi-forum" value="messages"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-  
-        <v-navigation-drawer permanent>
-          <v-list>
+          
             <v-list-item @click="router.push('/')">Home</v-list-item>
             <v-list-item @click="router.push('/profile')">Edit Profile</v-list-item>
 
             
             <v-btn @click="authStore.logout">Logout</v-btn>
-   
-           
           </v-list>
         </v-navigation-drawer>
+  
+
 </template>
 
 <style scoped>
