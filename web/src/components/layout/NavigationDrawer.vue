79f8b0a772f3968caaf97118ef0385a4b73db7c4
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-03-16 15:07:44 -0300

    feat: add icons to nav drawers

--- web/src/components/layout/NavigationDrawer.vue
+++ web/src/components/layout/NavigationDrawer.vue
@@ -23,13 +23,16 @@ const userStore = useUserStore()
             density="compact"
             nav
           >
-          
-            <v-list-item @click="router.push('/')">Home</v-list-item>
-            <v-list-item @click="router.push('/profile')">Edit Profile</v-list-item>
+    
+            <v-list-item append-icon="mdi-home" class="mt-10 mb-10" @click="router.push('/')">Home</v-list-item>
+ 
+            <v-list-item append-icon="mdi-account" class="mt-10 mb-10" @click="router.push('/profile')">Edit Profile</v-list-item>
+      
 
+        
             
-            <v-btn @click="authStore.logout">Logout</v-btn>
           </v-list>
+          <v-btn class="mt-16" @click="authStore.logout">Logout</v-btn>
         </v-navigation-drawer>
   
 
