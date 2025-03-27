b058e5c89ad4aaa9be986ca93f48ec957dbe7796
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-03-27 20:59:30 -0300

    fix: increase timeout time

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", port=5000, log_level="info", reload=True)
+    uvicorn.run("app:app", port=5000, log_level="info", reload=True, timeout_graceful_shutdown=10000, timeout_keep_alive=10000)
