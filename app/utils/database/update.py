995a6bc6ef40bf23fb27fcb15a9bc12c7b2c54a5
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-04-07 20:26:07 -0300

    fix: some corrections to update_document_object_instance

--- app/utils/database/update.py
+++ app/utils/database/update.py
@@ -6,16 +6,17 @@ def update_document_object_instance(database, object_id: str, updated_object):
     
     updated_object = {k: v for k, v in updated_object.dict().items() if v is not None}
     if len(updated_object) >= 1:
+        print('object_id', object_id, 'typ', type(object_id))
         update_result = database.update_one(
-            {"_id": ObjectId(id)}, {"$set": updated_object}
+            {"_id": ObjectId(object_id)}, {"$set": updated_object}
         )
 
         if update_result.modified_count == 0:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Object with ID {id} not found")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Object with ID {object_id} not found")
 
     if (
-        existing_object := database.find_one({"_id": ObjectId(id)})
+        existing_object := database.find_one({"_id": ObjectId(object_id)})
     ) is not None:
         return existing_object
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Object with ID {id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Object with ID {object_id} not found")
