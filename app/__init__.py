44febee1308345e762236870af6d780a68416649
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-04-18 14:58:33 -0300

    refactor: remove not used files from root folder

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from .database import register_db_connection
 from .routers import register_routers
 from .middlewares import register_middlewares
 
-import firebase_admin
 from pathlib import Path
 
 import os
--- start_dev.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call C:\Users\jvito\Desktop\enviroments\startstratum_core\Scripts\activate
-cd web
-npm run dev-on
