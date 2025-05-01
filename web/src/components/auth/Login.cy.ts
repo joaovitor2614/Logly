1d6a6a8cd9a31ffa6cc5b89f61df23aacb7a5d03
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-05-01 17:14:35 -0300

    feat: add vuetify plugin in cypress test

--- web/src/components/auth/Login.cy.ts
+++ web/src/components/auth/Login.cy.ts
@@ -1,8 +1,17 @@
 import Login from './Login.vue'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 describe('<Login />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-vue
-    cy.mount(Login)
+    cy.mount(Login, { global: { plugins: [vuetify] } })
   })
 })
