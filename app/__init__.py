20882466a250bc3d38e0822f52f4e0e6e6d35dd4
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-05-04 19:09:49 -0300

    feat: use fastapi lifespan to connect and clean db connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI
-from .database import register_db_connection
+from .database import db_lifespan
 from .routers import register_routers
 from .middlewares import register_middlewares
 
@@ -11,9 +11,11 @@ import os
 
 def create_app() -> FastAPI:
 
-    app = FastAPI()
-
-    register_db_connection(app)
+    app = FastAPI(
+        title="Mamata ou Cilada",
+        version="1.0.0",
+        lifespan=db_lifespan
+    )
      
     register_middlewares(app)
     register_routers(app)
--- app/database/__init__.py
+++ app/database/__init__.py
@@ -3,15 +3,11 @@ from pymongo import MongoClient, server_api
 from app.settings import APP_SETTINGS
 import os
 
-def register_db_connection(app: FastAPI):
-    @app.on_event("startup")
-    def startup_db_client():
-        app.mongodb_client = MongoClient(APP_SETTINGS.MONGO_DB_ATLAS_URI, server_api=server_api.ServerApi(version="1", strict=True, deprecation_errors=True))
-        app.database = app.mongodb_client[APP_SETTINGS.USERS_DB_NAME]
-        app.mongodb_client.admin.command("ping")
 
-        print("Connected to the MongoDB database!")
+def db_lifespan(app: FastAPI):
+    app.mongodb_client = MongoClient(APP_SETTINGS.MONGO_DB_ATLAS_URI, server_api=server_api.ServerApi(version="1", strict=True, deprecation_errors=True))
+    app.database = app.mongodb_client[APP_SETTINGS.USERS_DB_NAME]
+    app.mongodb_client.admin.command("ping")
+    yield
+    app.mongodb_client.close()
 
-    @app.on_event("shutdown")
-    def shutdown_db_client():
-        app.mongodb_client.close()
