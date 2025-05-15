95a47689e48bf7ce824e8d7516b864070714e88b
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-05-14 22:27:34 -0300

    refactor: genereate male and female gender in  create_new_fake_professor

--- app/utils/professor.py
+++ app/utils/professor.py
@@ -6,11 +6,13 @@ import random
 def create_new_fake_professor(faker_instance: Faker | None = None) -> Professor:
     if faker_instance is None:
         faker_instance = Faker()
+
+    professor_gender = 'male' if random.random() >= 0.5  else 'female'
     return Professor(
         name=faker_instance.name(),
         image='https://thispersondoesnotexist.com/',
         disciplines=['Calculo IV'],
-        gender='other',
+        gender=professor_gender,
         email=faker_instance.email(),
         phone=faker_instance.phone_number()
     )
