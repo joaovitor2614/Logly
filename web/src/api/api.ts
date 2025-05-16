f804308354b0a70481a915da62ec668413ba188e
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-05-16 14:40:06 -0300

    refactor: get api base url from env var

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import registerResponseInterceptors from './interceptors';
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 10000,
 });
 
