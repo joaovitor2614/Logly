cd0d1ae36add9fb76a5ac6384e8314b0dce8baaf
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-05-22 18:28:36 -0300

    feat: add env variable to name db

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app() -> FastAPI:
     app = FastAPI(
         title="Logly",
         version="1.0.0",
+        lifespan=db_lifespan
     )
      
     register_middlewares(app)
--- app/database/__init__.py
+++ app/database/__init__.py
@@ -6,7 +6,7 @@ import os
 
 def db_lifespan(app: FastAPI):
     app.mongodb_client = MongoClient(APP_SETTINGS.MONGO_DB_ATLAS_URI, server_api=server_api.ServerApi(version="1", strict=True, deprecation_errors=True))
-    app.database = app.mongodb_client[APP_SETTINGS.USERS_DB_NAME]
+    app.database = app.mongodb_client[APP_SETTINGS.DB_NAME]
     app.mongodb_client.admin.command("ping")
     yield
     app.mongodb_client.close()
