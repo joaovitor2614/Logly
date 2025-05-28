031114bd2ea168840711abd93606ae9b1a827f0e
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-05-28 20:03:42 -0300

    fix: use @asynccontextmanager in fastapi lifespan

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from pymongo import MongoClient, server_api
 from app.settings import APP_SETTINGS
+from contextlib import asynccontextmanager
 import os
 
-
+@asynccontextmanager
 def db_lifespan(app: FastAPI):
     app.mongodb_client = MongoClient(APP_SETTINGS.MONGO_DB_ATLAS_URI, server_api=server_api.ServerApi(version="1", strict=True, deprecation_errors=True))
     app.database = app.mongodb_client[APP_SETTINGS.DB_NAME]
