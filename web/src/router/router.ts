b7fbeed6ecd248f609132f53819d1f50f75130f7
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-05-29 18:04:54 -0300

    feat: create BillingPlanSelection comp

--- web/src/router/router.ts
+++ web/src/router/router.ts
@@ -4,6 +4,7 @@ import Dashboard from '../Dashboard.vue'
 import Register from '../components/auth/Register.vue'
 import Login from '../components/auth/Login.vue'
 import LandingPage from '../components/landing/LandingPage.vue'
+import BillingPlanSelection from '../components/billing/BillingPlanSelection.vue'
 import { registerRouteGuard } from './guard/index'
 import type { App } from 'vue';
 
@@ -24,6 +25,11 @@ const routes = [
     path: '/',
     name: 'Landing',
     component: LandingPage
+  },
+  {
+    path: '/billing',
+    name: 'Billing',
+    component: BillingPlanSelection
   },
   {
     path: '/dashbaord',
