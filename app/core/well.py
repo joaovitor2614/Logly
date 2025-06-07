f21873c17940a494207dcf28f655fd112e6778ab
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-06-07 20:29:21 -0300

    test: 3/4 add test to import well file

--- app/core/well.py
+++ app/core/well.py
@@ -1,6 +1,6 @@
 import lasio
 from app.models.well.well import WellLog
-
+from typing import List
 
 
 class WellHandler:
@@ -13,18 +13,35 @@ class WellHandler:
         
         self.lasio_object = lasio.read(self.las_file_path)
 
-        welllogs = self.create_well_logs_instances_from_curves_section()
-        
-        print('WELL', welllogs)
+        welllogs = self._create_well_logs_instances_from_curves_section()
 
-    def create_well_logs_instances_from_curves_section(self):
+        well = self._create_well_instance(welllogs)
+
+        return well
+        
+    def _create_well_instance(self, welllogs: List[WellLog]):
+        well_name = self.lasio_object.sections["Well"]["WELL"].value
+        return Well(name=well_name, welllogs=welllogs)
+       
+       
+
+    def _create_well_logs_instances_from_curves_section(self):
         curves_section = self.lasio_object.sections["Curves"]
-        return [
+        welllogs = [
             WellLog(
                 mnemonic =curve_item.mnemonic,
                 unit=curve_item.unit,
                 descr=curve_item.descr
             ) for curve_item in curves_section
         ]
+        self._populate_well_logs_instances_data(welllogs)
+        return welllogs
+            
 
+    def _populate_well_logs_instances_data(self, welllogs: List[WellLog]):
+        well_logs_amount = self.lasio_object.data.shape[1]
+        for i in range(well_logs_amount):
+            welllogs[i].data = self.lasio_object.data[:, i]
+ 
+    
 
