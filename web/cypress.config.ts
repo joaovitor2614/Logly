4ec8711d8af657bd77a1c4bbf65fd719a522a0d3
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-06-15 12:12:53 -0300

    feat: setup e2e tests cypress config

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -1,10 +1,16 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   component: {
     devServer: {
-      framework: 'vue',
-      bundler: 'vite',
+      framework: "vue",
+      bundler: "vite",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
