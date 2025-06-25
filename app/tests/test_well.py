8a8fc62cc09558c3a248bd025f0d07a0835b62fa
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-06-24 22:44:08 -0300

    test: 3/4 devevelop test_get_wells

--- app/tests/test_well.py
+++ app/tests/test_well.py
@@ -20,10 +20,10 @@ def test_import_well_file(client, import_well_file):
 def test_get_wells(client, import_well_file):
     _, request_headers = import_well_file
     well_endpoint_wrapper = WellEndPointWrapper(client, request_headers)
-    #response = well_endpoint_wrapper.get_wells()
-    #assert response.status_code == 200
-    #well_db_objs_jsonfied = response.json()
-    #assert len(well_db_objs_jsonfied) == 1
+    response = well_endpoint_wrapper.get_wells()
+    assert response.status_code == 200
+    well_db_objs_jsonfied = response.json()
+    assert len(well_db_objs_jsonfied) == 1
 
 
     
