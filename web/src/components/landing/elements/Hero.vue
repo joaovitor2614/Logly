82ffe0e6ced4f9210c3c672146e33dd9cedf6dea
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-08-02 18:28:00 -0300

    feat: 1/14 implement Landing Page

---
+++ web/src/components/landing/elements/Hero.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="untree_co-hero" id="home-section">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <div class="dots"></div>
+                    <div class="row align-items-center">
+                        <!--
+                        <div
+                        class="col-lg-7 ml-auto order-lg-2"
+                        data-aos="fade-right"
+                        data-aos-delay="400"
+                        >
+                        </div>
+                        -->
+                    </div>
+                    <div class="col-lg-5">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
