6bb3da15363757683ce1d0986f7866f0f4cf2f4a
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-08-03 18:24:16 -0300

    feat: config deploy 1/3

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", port=5000, log_level="info", reload=True)
+    uvicorn.run("app:app", port=5000, host="0.0.0.0", log_level="info")
