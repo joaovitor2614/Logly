02039eacbf27eb77acd7f1eaab058c88e44acf0a
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-08-12 14:18:57 -0300

    feat: adapt usePlotStore to support histo and scatter plots tempaltes

--- web/src/stores/plot.ts
+++ web/src/stores/plot.ts
@@ -1,17 +1,42 @@
+import { PlotType } from '@/components/plot/types'
 import { defineStore } from 'pinia'
 import { ref, type Ref } from 'vue'
 
+interface CrossPlotTemplate {
+    wellID: string,
+    xWellLogID: string,
+    yWellLogID: string
+}
 
+interface HistogramTemplate extends Omit<CrossPlotTemplate, 'yWellLogID'>{}
 
 export const usePlotStore = defineStore('plotStore', () => {
+    const crossPlotTemplate = ref<CrossPlotTemplate>({
+        wellID: '',
+        xWellLogID: '',
+        yWellLogID: ''
+    })
+    const histogramTemplate = ref<HistogramTemplate>({
+        wellID: '',
+        xWellLogID: ''
+    })
+    
     const xAxisSelectedWellLog: Ref<string> = ref('');
     const yAxisSelectedWellLog: Ref<string> = ref('');
     const wellID: Ref<string> = ref('');
 
-    const registerPlot = (xAxisLogID: string, yAxisLogID: string, selectedWellID: string) => {
-        xAxisSelectedWellLog.value = xAxisLogID;
-        yAxisSelectedWellLog.value = yAxisLogID;
-        wellID.value = selectedWellID
+    const plotTemplateByType: Record<PlotType, Ref<CrossPlotTemplate> | Ref<HistogramTemplate>> = {
+        'histogram': histogramTemplate,
+        'scatter': crossPlotTemplate
+    }
+    const registerPlot = (plotTeplate: CrossPlotTemplate, plotType: `${PlotType}`) => {
+        const template = plotTemplateByType[plotType]
+        template.value.wellID = plotTeplate.wellID
+        template.value.xWellLogID = plotTeplate.xWellLogID
+        
+        if (plotType === 'scatter') {
+            template.value.yWellLogID = plotTeplate.yWellLogID
+        }
     }
 
 
