c101c3f6b5e35dfcd3badb039bb252cf8c84b7ea
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-09-05 19:27:10 -0300

    refactor: simplify get well log data endpoint logic

--- web/src/components/plot/view/PlotView.vue
+++ web/src/components/plot/view/PlotView.vue
@@ -18,17 +18,12 @@ const wellStore = useWellStore()
 
 const template = props.plotType === 'histogram' ? plotStore.histogramTemplate : plotStore.crossPlotTemplate;
 
-const parseWellLogDataIntoValidJsArray = (stringfiedData: string) => {
-
-    const cleaned = stringfiedData.replace(/^"|"$/g, "");
+const convertStringfiedArrayToArray = (stringfiedData: string) => {
+    return stringfiedData
+    .split(",")                
+    .map(s => s.trim())       
+    .map(s => s === "NaN" ? NaN : parseFloat(s)); 
     
-    const arr = cleaned
-    .split(",")                // break into pieces
-    .map(s => s.trim())        // remove spaces
-    .map(s => s === "NaN" ? NaN : parseFloat(s)); // convert numbers, keep NaN
-    
-    return arr
-
 }   
 
 const fetchWellLogData =  async () => {
@@ -52,7 +47,7 @@ const plotWellLogData = (plotData: Record<string, Array<Number>>) => {
     const wellInfo = wellStore.wells.find(well => well._id === template.wellID)
     const xWellLogName = wellInfo?.welllogs.find(wellLog => wellLog._id === template.xWellLogID)?.name
     const yWellLogName = props.plotType === 'scatter' ? wellInfo?.welllogs.find(wellLog => wellLog._id === template.yWellLogID)?.name : ''
-    console.log('names', xWellLogName, yWellLogName)
+
     const plotProvider = new PlotProvider(props.plotType, plotData, xWellLogName, yWellLogName)
     plotProvider.run()
 ;
@@ -60,7 +55,7 @@ const plotWellLogData = (plotData: Record<string, Array<Number>>) => {
 const getAxisWellLogData = async (wellLogID: string) => {
     const response = await getWellLogDataByIDs(wellLogID, template.wellID)
     if (response) {
-        const wellLogData = parseWellLogDataIntoValidJsArray(response.data.data)
+        const wellLogData = convertStringfiedArrayToArray(response.data.data)
         return wellLogData
     } else {
         return []
