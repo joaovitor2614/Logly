473b55a5143c00265e9b35fe74fd428fda77847c
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-09-05 19:11:01 -0300

    refactor: modularizar lógica de template de PlotSection.vue

---
+++ web/src/components/plot/view/PlotCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { PlotType } from '../types';
+import PlotView from './PlotView.vue';
+interface Props {
+    plotType: `${PlotType}`,
+    title: string
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+     
+      <v-col xs="12" md="12">
+        <v-card outlined shaped elevation="2" class="pa-4">
+          <v-card-title class="font-weight-medium text-title-1">
+            {{ props.title }}
+          </v-card-title>
+          <v-divider></v-divider>
+
+          <v-card-text class="d-flex justify-start align-center">
+          
+            <div
+            >
+              <PlotView :plotType="props.plotType" />
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+</template>
+
+<style scoped>
+
+</style>
--- web/src/components/plot/view/PlotSection.vue
+++ web/src/components/plot/view/PlotSection.vue
@@ -1,48 +1,13 @@
 <template>
   <v-container fluid>
-
-
-
-
-      <v-col xs="12" md="12">
-        <v-card outlined shaped elevation="2" class="pa-4">
-          <v-card-title class="font-weight-medium text-title-1">
-            Crossplot
-          </v-card-title>
-          <v-divider></v-divider>
-
-          <v-card-text class="d-flex justify-start align-center" style="height: 350px;">
-      
-            <div
-            >
-              <PlotView :plotType="'scatter'" />
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
-
- 
-      <v-col xs="12" md="12">
-        <v-card outlined shaped elevation="2" class="pa-4">
-          <v-card-title class="font-weight-medium text-title-1">
-            Histogram
-          </v-card-title>
-          <v-divider></v-divider>
-
-          <v-card-text class="d-flex justify-start align-center">
-          
-            <div
-            >
-              <PlotView :plotType="'histogram'" />
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-col>
+      <PlotCard :plotType="'scatter'" :title="'Crossplot'" />
+      <PlotCard :plotType="'histogram'" :title="'Histogram'" />
 
   </v-container>
 </template>
 
 <script setup>
+import PlotCard from './PlotCard.vue';
 import PlotView from './PlotView.vue';
 
 
