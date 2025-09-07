2199bf199c9ab7f9e15cb4585b7da62f166a0d96
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-09-07 15:40:39 -0300

    refactor: turn register test into class

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -1,44 +1,40 @@
 from faker import Faker
-from app.settings import APP_SETTINGS
 import json
 from faker import Faker
 from ..utils.security import verify_password
 from app.tests.utils.wrapper.auth import AuthEndPointWrapper
 from app.controllers.user import UserController
 from app.tests.utils.auth import get_user_id_from_register_response
-from fastapi import  HTTPException, Response
 import pytest
 
-def test_register_user(client):
-
-    """
-    Test the user registration endpoint.
-
-    This test ensures that when a new user is registered using the provided
-    client and registration data, the response status code is 201, indicating 
-    successful creation. It also checks that a token is returned in the 
-    response JSON, confirming that the user has been authenticated.
-    """
-    fake = Faker()
-    user_controller = UserController(client)
-    auth_endpoint_mocker = AuthEndPointWrapper(client)
-    mock_new_user_data = {"name": fake.name(), "email": fake.email(), "password": fake.password()}
-    response = auth_endpoint_mocker.register(mock_new_user_data)
 
 
-    assert response.status_code == 201, f"Response status code expected to be 201, but got {response.status_code}"
-    response_content = response.json()
+class TestRegister:
+    @pytest.fixture(autouse=True)
+    def setup(self, client):
+        self.client = client
+        self.auth_endpoint_mocker = AuthEndPointWrapper(self.client)
+        self.user_controller = UserController(client)
 
-    assert response_content["token"] is not None, "Token not present auth endpoint response."
+    def test_register_user(self):
+        fake = Faker()
+        mock_new_user_data = {"name": fake.name(), "email": fake.email(), "password": fake.password()}
+        response = self.auth_endpoint_mocker.register(mock_new_user_data)
 
 
-    user_id = get_user_id_from_register_response(response)
+        assert response.status_code == 201, f"Response status code expected to be 201, but got {response.status_code}"
+        response_content = response.json()
 
-    new_user = user_controller.get_user_by_id(user_id)
-    
-    assert verify_password(mock_new_user_data["password"], new_user["password"]), "Password not hashed correctly"
-    assert new_user["email"] == mock_new_user_data["email"], "Email not stored correctly"
-    assert new_user["name"] == mock_new_user_data["name"], "Name not stored correctly"
+        assert response_content["token"] is not None, "Token not present auth endpoint response."
+
+        
+        user_id = get_user_id_from_register_response(response)
+
+        new_user = self.user_controller.get_user_by_id(user_id)
+        
+        assert verify_password(mock_new_user_data["password"], new_user["password"]), "Password not hashed correctly"
+        assert new_user["email"] == mock_new_user_data["email"], "Email not stored correctly"
+        assert new_user["name"] == mock_new_user_data["name"], "Name not stored correctly"
 
 
    
