88dc6395e9bb953c8c2c82ab2a1626b98b2b9eae
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2025-10-09 21:09:41 -0300

    feat: 1/2 plot well log in table in comp WellLogDataDisplay

--- web/src/components/welllog/WellLogDataDisplay.vue
+++ web/src/components/welllog/WellLogDataDisplay.vue
@@ -1,29 +1,61 @@
 <script setup lang="ts">
-import {onBeforeMount, ref, Ref} from 'vue'
+import {computed, onBeforeMount, ref, Ref} from 'vue'
 import { getParsedWellLogByID } from '@/utils/well';
 import { useRoute } from 'vue-router';
+import { useWellStore } from '@/stores';
+
+const wellStore = useWellStore()
+
+const currentWellLogInfo: Ref<App.Well.WellLog | undefined> = ref(undefined)
+
+const wellLogDataTableHeaders = computed(() => {
+    return [
+        {
+            title: `${currentWellLogInfo.value?.name} ${currentWellLogInfo.value?.unit}`,
+            key: 'data',
+            align: 'center'
+    }]
+})
 
 
 const route = useRoute()
 const currentWellLogData: Ref<Number[]> = ref([])
+const wellLogDataTableItems = computed(() => {
+    return []
+})
 const setWellLogDataToDisplay = async () => {
     const {well_id, well_log_id } = route.params
 
  
-    currentWellLogData.value = await getParsedWellLogByID(well_log_id, well_id)
-    console.log('currentWellLogData.value', currentWellLogData.value)
+    currentWellLogData.value = await getParsedWellLogByID(well_log_id as string, well_id as string)
+    
 }
 
-onBeforeMount(() => {
-    setWellLogDataToDisplay()
+const setWellLogInfo = () => {
+    const selectedWell = wellStore.wells.find(well => well._id === route.params.well_id);
+    currentWellLogInfo.value = selectedWell?.welllogs.find(wellLog => wellLog._id === route.params.well_log_id);
+    
+}
 
+onBeforeMount(async () => {
+    setWellLogInfo()
+    await setWellLogDataToDisplay()
 })
 </script>
 
 <template>
-    <div>
-
-    </div>
+    <v-container class="d-flex flex-column justify-center">
+                <v-data-table
+                    class="mt-2 mb-10"
+                    :headers="wellLogDataTableHeaders"
+                    :items="wellLogDataTableItems"
+                    :id="'test-well-table'"
+                 
+                    hide-default-footer
+                >
+            
+                </v-data-table>
+    </v-container>
 </template>
 
 <style scoped>
