eb79221f55e09550f5389eecf233e80957a42b8b
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2026-01-11 11:38:02 -0300

    feat: add btns to well calculator window

--- web/src/components/well/calculator/WellCalculatorDialog.vue
+++ web/src/components/well/calculator/WellCalculatorDialog.vue
@@ -1,12 +1,18 @@
 vue3<script setup>
 import WellSelector from '@/components/well/helpers/WellSelector.vue';
 import DialogWrapper from '@/components/common/DialogWrapper.vue';
-
+import Button from '@/components/common/Button.vue'
 import {  reactive } from 'vue';
+import { useDialogStore } from '@/stores';
 
 const form = reactive({
     wellID: ''
 })
+
+const dialogStore = useDialogStore()
+const executeCalculation = () => {
+    console.log('executeCalculation')
+}
 </script>
 
 <template>
@@ -14,6 +20,19 @@ const form = reactive({
         <v-container class="d-flex flex-column justify-center pa-12">
             <WellSelector v-model:wellID="form.wellID" />
         </v-container>
+        <v-card-actions>
+            <Button 
+            :buttonAction="dialogStore.closeDialogWindow"
+            >
+                Cancel
+            </Button>  
+            <Button 
+                :button-action="executeCalculation"
+                
+                >Calculate
+            </Button> 
+        </v-card-actions>     
+
 
    
           
