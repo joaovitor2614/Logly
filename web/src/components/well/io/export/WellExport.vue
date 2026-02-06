c5d4481890904021513e28cf60b508d48d65d294
JOÃO VITOR ALVES ESTRELLA <72946113+joaovitor2614@users.noreply.github.com>
2026-02-05 22:01:42 -0300

    feat: make well export dialog open

---
+++ web/src/components/well/io/export/WellExport.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import DialogWrapper from '@/components/common/DialogWrapper.vue';
+import WellIO from '../WellIO.vue';
+
+
+
+
+
+
+
+
+
+
+
+</script>
+
+<template>
+    <DialogWrapper cardTitle="Export Well">
+                <WellIO :wellIOType="'export'"/>
+
+
+     
+        
+        </DialogWrapper >
+</template>
+
+<style scoped>
+
+</style>
--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,7 @@ import "vue-toastification/dist/index.css";
 import '@mdi/font/css/materialdesignicons.css'
 import "./assets/main.css";
 import WellImport from '@/components/well/io/import/WellImport.vue';
+import WellExport from '@/components/well/io/export/WellExport.vue';
 import ConfirmDeleteAccountDialog from "@/components/account/ConfirmDeleteAccountDialog.vue";
 import PlotDialog from "@/components/plot/dialog/PlotDialog.vue";
 import LogPlotDialog from "@/components/plot/dialog/LogPlotDialog.vue";
@@ -49,6 +50,7 @@ app.component('PlotDialog', PlotDialog)
 app.component('LogPlotDialog', LogPlotDialog)
 app.component('WellCalculatorDialog', WellCalculatorDialog)
 app.component('WellImport', WellImport)
+app.component('WellExport', WellExport)
 app.component('ConfirmDeleteAccountDialog', ConfirmDeleteAccountDialog)
 app.component('WellLogsTableDialog', WellLogsTableDialog)
 app.mount("#app");
